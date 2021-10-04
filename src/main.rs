e41e737347920118f59adebfda1dc5b76bd50146
Kyle 'Klutch' Leary <klutchdevelopment@gmail.com>
2021-10-04 17:29:29 -0400

    Added basic concepts / comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// Globally scoped constant
+// const is equivalent to:
+// TS: const globalVariable:String = "constant";
+// Dart: const final String globalVariable = "constant";
+// const GLOBAL_VARIABLE: &str = "global constant";
+
 fn main() {
-    println!("Hello, world!");
+    // Equivalent to console.log(), printf(), print()
+    println!("Hi, Mom!");
+
+    // Equivalent to const (js), final/static (dart)
+    // let some_variable = "immutable";
+
+    // Equivalent to let(ts because you can't change the type)
+    // let mut another_variable = "mutable";
+
+    // Locally scoped constant
+    // const LOCAL_VARIABLE: &str = "local constant";
 }
